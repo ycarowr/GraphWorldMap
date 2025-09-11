515934cf7d8cf209cee30d7eb0a8aed33ac56006
ycarowr <ywrycaro@gmail.com>
2025-09-11 22:54:01 +0200

    Add proper generation for startending

--- Assets/Tools/WorldMap/Runtime/Core/WorldMap.cs
+++ Assets/Tools/WorldMap/Runtime/Core/WorldMap.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Tools.WorldMapCore.Database;
 using UnityEngine;
 
 namespace Tools.WorldMapCore.Runtime
@@ -17,8 +18,10 @@ namespace Tools.WorldMapCore.Runtime
 
         private readonly WorldMapStaticData Data;
         private readonly Dictionary<EDeletionReason, List<WorldMapNode>> Deletions;
+        public readonly List<WorldMapNode> End;
         public readonly List<WorldMapNode> Nodes;
         public readonly WorldMapRandom Random;
+        public readonly List<WorldMapNode> Start;
 
         public WorldMap(WorldMapStaticData data)
         {
@@ -26,12 +29,76 @@ namespace Tools.WorldMapCore.Runtime
             WorldMapHelper.ResetID();
             Random = new WorldMapRandom(Data);
             Nodes = new List<WorldMapNode>();
+            Start = new List<WorldMapNode>();
+            End = new List<WorldMapNode>();
             Deletions = new Dictionary<EDeletionReason, List<WorldMapNode>>
             {
                 { EDeletionReason.OutOfBounds, new List<WorldMapNode>() },
                 { EDeletionReason.Overlap, new List<WorldMapNode>() },
                 { EDeletionReason.Isolation, new List<WorldMapNode>() },
             };
+
+            // Generate Starting
+            for (var index = 0; index < Data.AmountStart; index++)
+            {
+                var worldPosition = Vector2.zero;
+                if (Data.Orientation == WorldMapParameters.Orientation.Horizontal)
+                {
+                    var segment = Data.WorldBounds.size.y / (Data.AmountStart + 1);
+                    worldPosition.x = Data.WorldBounds.min.x - Data.NodeWorldSize.x / 2 -
+                                      WorldMapParameters.SMALL_NUMBER;
+                    worldPosition.y = segment * (index + 1);
+                }
+
+                if (Data.Orientation == WorldMapParameters.Orientation.Vertical)
+                {
+                    var segment = Data.WorldBounds.size.x / (Data.AmountStart + 1);
+                    worldPosition.x = segment * (index + 1);
+                    worldPosition.y = Data.WorldBounds.min.y - Data.NodeWorldSize.y / 2 -
+                                      WorldMapParameters.SMALL_NUMBER;
+                }
+
+                if (worldPosition != Vector2.zero)
+                {
+                    var generated = GenerateNodeAt(worldPosition, true);
+                    if (generated != null)
+                    {
+                        Start.Add(generated);
+                        Nodes.Add(generated);
+                    }
+                }
+            }
+
+            // Generate Ending
+            for (var index = 0; index < Data.AmountEnd; index++)
+            {
+                var worldPosition = Vector2.zero;
+                if (Data.Orientation == WorldMapParameters.Orientation.Horizontal)
+                {
+                    var segment = Data.WorldBounds.size.y / (Data.AmountEnd + 1);
+                    worldPosition.x = Data.WorldBounds.max.x + Data.NodeWorldSize.x / 2 +
+                                      WorldMapParameters.SMALL_NUMBER;
+                    worldPosition.y = segment * (index + 1);
+                }
+
+                if (Data.Orientation == WorldMapParameters.Orientation.Vertical)
+                {
+                    var segment = Data.WorldBounds.size.x / (Data.AmountEnd + 1);
+                    worldPosition.x = segment * (index + 1);
+                    worldPosition.y = Data.WorldBounds.max.y + Data.NodeWorldSize.y / 2 +
+                                      WorldMapParameters.SMALL_NUMBER;
+                }
+
+                if (worldPosition != Vector2.zero)
+                {
+                    var generated = GenerateNodeAt(worldPosition, true);
+                    if (generated != null)
+                    {
+                        End.Add(generated);
+                        Nodes.Add(generated);
+                    }
+                }
+            }
         }
 
 
@@ -48,35 +115,49 @@ namespace Tools.WorldMapCore.Runtime
         public void GenerateNodes()
         {
             var amountToCreate = Data.Amount;
-            var worldSize = Data.NodeWorldSize;
             var count = 0;
             var maxCount = Mathf.Max(Data.Iterations, amountToCreate);
             while (Nodes.Count != amountToCreate && count < maxCount)
             {
                 count++;
                 var worldPosition = Random.GenerateRandomPosition(Data);
-                var newNode = new WorldMapNode(WorldMapHelper.GenerateID(), worldPosition, worldSize);
-
-                if (WorldMapHelper.CheckOverlap(newNode, Nodes))
+                var generated = GenerateNodeAt(worldPosition);
+                if (generated != null)
                 {
-                    if (WorldMapHelper.CheckBounds(newNode, Data))
-                    {
-                        Nodes.Add(newNode);
-                    }
-                    else
-                    {
-                        Deletions[EDeletionReason.OutOfBounds].Add(newNode);
-                    }
-                }
-                else
-                {
-                    Deletions[EDeletionReason.Overlap].Add(newNode);
+                    Nodes.Add(generated);
                 }
             }
 
             Nodes.Sort();
             var isolationNodes = Deletions[EDeletionReason.Isolation];
             WorldMapHelper.CheckIsolationDistance(Nodes, Data, ref isolationNodes);
+        }
+
+        private WorldMapNode GenerateNodeAt(Vector2 worldPosition, bool skipChecks = false)
+        {
+            var worldSize = Data.NodeWorldSize;
+            var newNode = new WorldMapNode(WorldMapHelper.GenerateID(), worldPosition, worldSize);
+
+            if (skipChecks)
+            {
+                return newNode;
+            }
+
+            if (WorldMapHelper.CheckOverlap(newNode, Nodes))
+            {
+                if (WorldMapHelper.CheckBounds(newNode, Data))
+                {
+                    return newNode;
+                }
+
+                Deletions[EDeletionReason.OutOfBounds].Add(newNode);
+            }
+            else
+            {
+                Deletions[EDeletionReason.Overlap].Add(newNode);
+            }
+
+            return null;
         }
 
 #if UNITY_EDITOR
