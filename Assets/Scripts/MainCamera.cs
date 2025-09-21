9b16dde3b290bdf1b6d99b6dc3505ffcc1e5770a
ycarowr <ywrycaro@gmail.com>
2025-09-21 20:43:44 +0200

    more fixes for camera

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,5 +1,6 @@
 using System;
 using Tools.Attributes;
+using Tools.WorldMapCore.Database;
 using UnityEngine;
 
 namespace Game
@@ -22,19 +23,64 @@ namespace Game
         [Button]
         public void OnCreateWorldMap()
         {
-            var cameraComponent = GetComponent<Camera>();
-            Vector3 position = gameWorldMap.WorldMap.Data.WorldBounds.center;
-            transform.position = position + offset;
-            var worldAspect = gameWorldMap.WorldMap.Data.WorldBounds.width / gameWorldMap.WorldMap.Data.WorldBounds.height; 
-            Debug.Log(cameraComponent.aspect + " " + worldAspect);
-            if (worldAspect > cameraComponent.aspect)
+            CentralizePosition();
+            SetOrthographicSize();
+        }
+
+        private void SetOrthographicSize()
+        {
+            var worldBounds = gameWorldMap.WorldMap.Data.WorldBounds;
+            var nodeSize = gameWorldMap.WorldMap.Data.Parameters.NodeWorldSize;
+            var worldAspect = CalcWorldAspect(worldBounds, nodeSize);
+            CalcOrthographicSize(worldAspect, worldBounds, nodeSize);
+        }
+
+        private float CalcWorldAspect(Rect worldBounds, Vector2 nodeSize)
+        {
+            float worldAspect;
+            if (gameWorldMap.WorldMap.Data.Parameters.Orientation == WorldMapParameters.OrientationGraph.LeftRight)
             {
-                cameraComponent.orthographicSize = gameWorldMap.WorldMap.Data.WorldBounds.width / (cameraComponent.aspect * 2) ;
+                worldAspect = (worldBounds.width + nodeSize.x) / worldBounds.height;
             }
             else
             {
-                cameraComponent.orthographicSize = gameWorldMap.WorldMap.Data.WorldBounds.height / 2f;   
+                worldAspect = worldBounds.width / (worldBounds.height + nodeSize.y);
             }
+
+            return worldAspect;
+        }
+
+        private void CalcOrthographicSize(float worldAspect, Rect worldBounds, Vector2 nodeSize)
+        {
+            var cameraComponent = GetComponent<Camera>();
+            if (gameWorldMap.WorldMap.Data.Parameters.Orientation == WorldMapParameters.OrientationGraph.LeftRight)
+            {
+                if (worldAspect > cameraComponent.aspect)
+                {
+                    cameraComponent.orthographicSize = (worldBounds.width + nodeSize.x * 2) / (cameraComponent.aspect * 2);
+                }
+                else
+                {
+                    cameraComponent.orthographicSize = (worldBounds.height) / 2f;
+                }
+            }
+            else
+            {
+                if (worldAspect > cameraComponent.aspect)
+                {
+                    cameraComponent.orthographicSize = (worldBounds.width + nodeSize.x * 2) / (cameraComponent.aspect * 2);
+                }
+                else
+                {
+                    cameraComponent.orthographicSize = (worldBounds.height + nodeSize.y * 2) / 2f;
+                }
+            }
+        }
+
+        private void CentralizePosition()
+        {
+            Vector3 position = gameWorldMap.WorldMap.Data.WorldBounds.center;
+            transform.position = position + offset;
         }
     }
 }
