bc6225b309178263677cc0f861438489e02a29d1
ycarowr <ywrycaro@gmail.com>
2025-09-28 10:18:59 +0200

    Add test polygon

---
+++ Assets/Scripts/Polygon.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Polygon : MonoBehaviour
+{
+    public Vector3[] vertices;
+
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLineStrip(vertices, true);
+        Gizmos.DrawWireSphere(CalcCentroid(), 1);
+    }
+
+    public Vector3 CalcCentroid()
+    {
+        if (vertices == null || vertices.Length < 3)
+        {
+            return Vector3.zero;
+        }
+        
+        var centroid = Vector3.zero;
+        foreach (var v in vertices)
+        {
+            centroid += v;
+        }
+        
+        centroid /= vertices.Length;
+
+        return centroid;
+    }
+}
